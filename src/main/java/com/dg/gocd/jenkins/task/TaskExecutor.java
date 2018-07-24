f39a8f992e010b52a2edad0f3c8aa786e96c4cc6
Dima Golomozy <dima.golomozy@gmail.com>
2018-07-24 23:23:43 +0300

    new Task executor with jenkins factory

--- src/main/java/com/dg/gocd/jenkins/task/TaskExecutor.java
+++ src/main/java/com/dg/gocd/jenkins/task/TaskExecutor.java
@@ -1,45 +1,50 @@
 package com.dg.gocd.jenkins.task;
 
+import com.dg.gocd.utiils.JenkinsServerFactory;
+import com.google.common.annotations.VisibleForTesting;
 import com.offbytwo.jenkins.JenkinsServer;
 import com.offbytwo.jenkins.model.Build;
 import com.offbytwo.jenkins.model.JobWithDetails;
 import com.offbytwo.jenkins.model.QueueItem;
 import com.offbytwo.jenkins.model.QueueReference;
-import com.thoughtworks.go.plugin.api.response.GoPluginApiResponse;
 import com.thoughtworks.go.plugin.api.task.JobConsoleLogger;
 
 import java.net.URI;
 
-import static com.dg.gocd.utiils.GoPluginApiUtils.successResponse;
+import static com.offbytwo.jenkins.model.BuildResult.SUCCESS;
 
 /**
  * @author dima.golomozy
  */
 public class TaskExecutor {
 
+    private final JenkinsServerFactory jenkinsServerFactory;
     private final JobConsoleLogger console;
-    private final TaskConfig taskConfig;
-    private final TaskContext taskContext;
 
-    public TaskExecutor(TaskConfig taskConfig, TaskContext taskContext) {
-        this(taskConfig, taskContext, JobConsoleLogger.getConsoleLogger());
+    public TaskExecutor() {
+        this(JenkinsServerFactory.getFactory(), JobConsoleLogger.getConsoleLogger());
     }
 
-    TaskExecutor(TaskConfig taskConfig, TaskContext taskContext, JobConsoleLogger console) {
-        this.taskConfig = taskConfig;
-        this.taskContext = taskContext;
+    @VisibleForTesting
+    TaskExecutor(JenkinsServerFactory jenkinsServerFactory, JobConsoleLogger console) {
+        this.jenkinsServerFactory = jenkinsServerFactory;
         this.console = console;
     }
 
-    public GoPluginApiResponse execute() throws Exception {
-        console.printLine("Executing request to url [" + taskConfig.getUrl() + "] with token ["  + taskConfig.getToken() + "]");
-        console.printLine("username [" + taskConfig.getUsername() + "], password [" + taskConfig.getPassword() + "], params " + taskConfig.getParams().toString());
-        return exec(new JenkinsServer(new URI(taskConfig.getUrl()), "", ""));
+    public TaskResult execute(TaskConfig taskConfig, TaskContext taskContext) {
+        console.printLine("Executing request to url [" + taskConfig.getUrl() + "] job ["  + taskConfig.getJob() + "]");
+
+        try (JenkinsServer jenkinsServer = jenkinsServerFactory.getJenkinsServer(new URI(taskConfig.getUrl()), taskConfig.getUsername(), taskConfig.getPassword())) {
+            return exec(jenkinsServer, taskConfig, taskContext);
+        } catch (Exception e) {
+            return new TaskResult(false, "Failed executing task", e);
+        }
     }
 
-    GoPluginApiResponse exec(JenkinsServer jenkinsServer) throws Exception {
-        JobWithDetails job = jenkinsServer.getJob("Dima-Test");
-        QueueReference queueReference = job.build();
+    private TaskResult exec(JenkinsServer jenkinsServer, TaskConfig taskConfig, TaskContext taskContext) throws Exception {
+        JobWithDetails job = jenkinsServer.getJob(taskConfig.getJob());
+        console.printLine("Building job...");
+        QueueReference queueReference = taskConfig.getParams().isEmpty() ? job.build() : job.build(taskConfig.getParams());
 
         QueueItem queueItem = jenkinsServer.getQueueItem(queueReference);
         while (queueItem.getExecutable() == null) {
@@ -47,16 +52,19 @@ public class TaskExecutor {
             Thread.sleep(2000);
 
             queueItem = jenkinsServer.getQueueItem(queueReference);
-        } while (queueItem.getExecutable() == null);
-
-        Build build = jenkinsServer.getBuild(queueItem);
-
-        while (build.details().isBuilding()) {
-            console.printLine("Job still running");
-            Thread.sleep(10000L);
         }
 
-        console.printLine("Result = " + build.details().getResult());
-        return successResponse(new TaskResult(true, "Hello").toMap());
+        Build build = jenkinsServer.getBuild(queueItem);
+        while (build.details().isBuilding()) {
+            console.printLine("Job still running");
+            Thread.sleep(10000);
+        }
+
+        console.printLine("Job done. Result = " + build.details().getResult());
+        console.printLine("Jenkins console output:\n" + build.details().getConsoleOutputText(0).getConsoleLog());
+        return new TaskResult(
+                SUCCESS.equals(build.details().getResult()),
+                "Jenkins Console log:\n" + build.details().getConsoleOutputText(0).getConsoleLog()
+        );
     }
 }
