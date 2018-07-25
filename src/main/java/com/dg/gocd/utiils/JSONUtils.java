ea5c1d01befd6c932f79c04577614e86b1873f0f
Dima Golomozy <dima.golomozy@gmail.com>
2018-07-25 19:40:42 +0300

    back to gson

--- src/main/java/com/dg/gocd/utiils/JSONUtils.java
+++ src/main/java/com/dg/gocd/utiils/JSONUtils.java
@@ -1,29 +1,19 @@
 package com.dg.gocd.utiils;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import java.io.IOException;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 /**
  * @author dima.golomozy
  */
 public final class JSONUtils {
-    private static final ObjectMapper mapper = new ObjectMapper();
+    private static final Gson gson = new GsonBuilder().serializeNulls().create();
 
     public static <T> T fromJSON(String json, Class<T> classOfT) {
-        try {
-            return mapper.readValue(json, classOfT);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        return gson.fromJson(json, classOfT);
     }
 
     public static <T> String toJSON(T obj) {
-        try {
-            return mapper.writeValueAsString(obj);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
+        return gson.toJson(obj);
     }
 }
